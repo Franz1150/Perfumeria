a20a6345b01b306d16092fd29aa15cc8a353e8cb
Johann Franz Niño Ibarra <82859618+Franz1150@users.noreply.github.com>
2025-11-06 19:50:18 -0500

    Create color.js

---
+++ color.js
@@ -0,0 +1,8 @@
+function cambiarColor(){
+    //1.Seleccionar elemento div por su ID.
+    let caja=document.getElementById('caja-colorida');
+
+    //2.usar classList para verificar y manipular clases CSS.
+    //Si la caja ya tiene la clase 'color-alternativo', la quita.
+    //Si no la tiene, la añade.
+    caja.classList.toggle('color-alternativo');
